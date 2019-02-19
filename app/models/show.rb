3db65561667ff27873d8bf0108911dad8cabfaf4
Stephen Wright <scw248@gmail.com>
2019-02-19 16:47:14 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,7 +5,7 @@ class Show < ActiveRecord::Base
   end
   
   def self.most_popular_show
-    self.find_by(:highest_rating)
+    self.find_by(highest_rating)
   end
   
   def self.lowest_rating
@@ -13,7 +13,7 @@ class Show < ActiveRecord::Base
   end
   
   def self.least_popular_show
-    self.find_by(:lowest_rating)
+    self.find_by(lowest_rating)
   end
   
 end
