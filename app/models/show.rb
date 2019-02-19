b22d94af04b751aaba753a13a191784ddee78b43
Stephen Wright <scw248@gmail.com>
2019-02-19 17:22:25 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,7 +5,7 @@ class Show < ActiveRecord::Base
   end
   
   def self.most_popular_show
-    self.find_by(highest_rating)
+    self.where("rating = #{highest_rating}").first
   end
   
   def self.lowest_rating
